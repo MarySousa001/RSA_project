272f4f040680396b619960e5d30da745cf13b1e4
MarySousa001 <78039209+MarySousa001@users.noreply.github.com>
2022-03-08 15:57:24 +0000

    d

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        RSA.keyGenerator();
+
+        boolean cont = true;
+        while (cont){
+            int num;
+            int numa;
+            Scanner aca = new Scanner(System.in);
+            RSA.keyGenerator();
+            System.out.println("Insert the message");
+            num = aca.nextInt();
+            RSA.encrypt(num);
+            System.out.println("Insert the d");
+            numa = aca.nextInt();
+            RSA.decrypt(numa);
+            cont = false;
+        }
+
+
     }
 }
--- src/RSA.java
+++ src/RSA.java
@@ -1,22 +1,36 @@
+import java.math.BigInteger;
 import java.util.Random;
 
 public class RSA {
-    public static boolean flag = false;
     public static int n;
     public static int p;
     public static int q;
     public static int dy;
-    public static int d;
+    public static int d = 0;
     public static int phi;
     public static int e;
-    public static int t;
 
-    public static int randPQ(){
+
+//    public static int randPQ() {
+//        int newNumber = 0;
+//        boolean a = false;
+//        Random random = new Random();
+//        int max = 499;
+//        int min = 3;
+//        while (!a) {
+//            int number = min + random.nextInt(max);
+//            if (prim(number)) {
+//                newNumber = number;
+//                a = true;
+//            }
+//        }
+//        return newNumber;
+//    }
+
+    public static int randN(int min, int max) {
         int newNumber = 0;
         boolean a = false;
         Random random = new Random();
-        int max = 999;
-        int min = 99;
         while (!a) {
             int number = min + random.nextInt(max);
             if (prim(number)) {
@@ -26,80 +40,105 @@ public class RSA {
         }
         return newNumber;
     }
-    public static int randN(){
-        int newNumber = 0;
-        boolean a = false;
-        Random random = new Random();
-        int max = 99;
-        int min = 1;
-        while (!a) {
-            int number = min + random.nextInt(max);
-            if (prim(number)) {
-                newNumber = number;
-                a = true;
-            }
-        }
-        return newNumber;
-    }
-    public static int gcd(int a, int b)
-    {
+
+    public static int gcd(int a, int b) {
         if (a == 0)
             return b;
-        return gcd(b%a, a);
+        return gcd(b % a, a);
     }
-    public static int keyGenerator(){
-        p = randPQ();
+
+    public static int keyGenerator() {
+        p = randN(3, 99);
         System.out.println("P - " + p);
-        int pq = randPQ();
-        while(p == pq){
-            pq = randPQ();
+        int pq = randN(3, 99);
+        while (p == pq) {
+            pq = randN(3, 99);
         }
         q = pq;
         System.out.println(("Q -" + q));
         n = p * q;
         System.out.println("N - " + n);
-        phi= (p-1) * (q-1);
-        System.out.println("Phi - "+ phi);
-        e = randN();
-        dy = gcd(e, phi);
-        while (flag) {
-            if (dy != 1) {
-                e = randN();
-            }
+        phi = (p - 1) * (q - 1);
+        System.out.println("Phi - " + phi);
+        e = randN(3, phi/3);
+        dy = gcd(phi, e);
+        while (dy != 1) {
+            e = randN(3, phi);
+            dy = gcd(phi, e);
             if (dy == 1) {
-                flag = true;
+                break;
+            }
+        }
+        System.out.println("Dy - " + dy);
+        System.out.println("E " + e);
+
+        int k =1;
+        double al;
+        int ei = e;
+        while(ei != 0) {
+            al = (double) (1 + phi*k) / e;
+                if(!isInteger(al)){
+                    k++;
+                    --ei;
+                }
+            else{
+                d = (int) al;
                 break;
             }
         }
 
-        System.out.println("Dy - "+dy);
-        System.out.println("E " + e);
-        System.out.println("("+ e + "," + n + ")");
-        d =  modInverse(e, phi);
-        if (d <0 ) d = phi+d;
-        System.out.println(d);
+//        d = modInverse(e, phi);
+//        if (d < 0) d = phi + d;
+        System.out.println("D " + d);
         return d;
     }
-    public static int modInverse(int a, int m)
-    {
 
+    public static int modInverse(int a, int m) {
         for (int x = 1; x < m; x++)
-            if (((a%m) * (x%m)) % m == 1)
+            if (((a % m) * (x % m)) % m == 1)
                 return x;
         return 1;
     }
-    public static boolean prim(int num){
-        for(int i = 2; i<num ; i++){
-            if (num % i == 0){
+
+    public static boolean prim(int num) {
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
                 return false;
             }
-        }return true;
+        }
+        return true;
     }
-    public static void encrypt(){
+    public static boolean isInteger(double N){
+        int X = (int) N;
+        double temp2 = N - X;
+        if(temp2 > 0){
+            return false;
+        }
+        return true;
+    }
+
+    public static void encrypt(int message) {
+        int Exp = e;
+        long la = 1;
+        while (Exp != 0){
+            la *= message;
+            -- Exp;
+        }
+        long ca = la % n;
+        System.out.println("C " + ca);
 
     }
-    public static void decrypt(){
 
+    public static void decrypt(int mess) {
+        int Exp = d;
+        BigInteger ai = BigInteger.valueOf(1);
+        while (Exp !=0){
+            //ai *= BigInteger.valueOf(mess);
+            --Exp;
+        }
+        long ma = ai % n;
+        System.out.println("M " + ma);
     }
+
 
 }
